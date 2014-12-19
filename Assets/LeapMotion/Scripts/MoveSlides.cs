6ebe86f423e8526a8d3e1776d674ef288b51d158
gembaf <s11t230@stmail.eng.kagawa-u.ac.jp>
2014-12-20 02:21:59 +0900

    手の動きによってスライドを左右に動かせるようにする

---
+++ Assets/LeapMotion/Scripts/MoveSlides.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class MoveSlides : MonoBehaviour {
+  private const float RATE = 0.0005f;
+
+  void Update () {
+    Frame frame = App.LeapController.Frame();
+    HandList hands = frame.Hands;
+    Hand firstHand = hands[0];
+    Vector position = firstHand.PalmVelocity;
+    transform.Translate(position.x * RATE, 0, 0);
+  }
+}
+
